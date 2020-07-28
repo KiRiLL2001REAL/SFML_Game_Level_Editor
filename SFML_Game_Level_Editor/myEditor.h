604ed17431864798b0ea8ab727c0b229e661bf8a
Kirill Baturin <batyrin2001@yandex.ru>
2020-07-28 21:28:23 +0300

    Class 'myDesktop' was transfered from 'main.cpp' to 'myEditor' files

---
+++ SFML_Game_Level_Editor/myEditor.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include "editor.h"
+
+const unsigned char code_about_paragraph = 100;
+
+class myDesktop : public edt::tDesktop {
+protected:
+	enum class screen_codes { Menu, MapEditor, NPCEditor };
+
+public:
+	myDesktop(std::string path_to_folder) : edt::tDesktop(path_to_folder) {};
+	~myDesktop() { std::cout << "~myDesktop done.\n"; };
+
+	virtual void changeScreen(char new_screen_code);
+	virtual void handleEvent(edt::tEvent& e);
+};
